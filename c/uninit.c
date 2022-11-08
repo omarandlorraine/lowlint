2d78dc80d8cc766c15d0106bc5516f7ea85aa3dd
Sam M W <you@example.com>
2022-11-08 21:14:09 +0000

    the uninitialized variable checker seems to be working okay

--- c/uninit.c
+++ c/uninit.c
@@ -2,4 +2,5 @@
 int main() {
 	int i;
 	putchar(i);
+	int x = i;
 }
--- src/init.rs
+++ src/init.rs
@@ -1,7 +1,7 @@
 use lang_c::visit::Visit;
 use lang_c::ast::InitDeclarator;
 use lang_c::span::Span;
-use lang_c::ast::DeclaratorKind::Identifier;
+use lang_c::ast::BinaryOperatorExpression;
 
 /// Checks that the variables have been initialized before their first use
 pub struct VarInitCheck {
@@ -22,14 +22,51 @@ impl<'ast> Visit<'ast> for VarInitCheck {
         init_declarator: &'ast InitDeclarator,
         span: &'ast Span
     ) {
+        use lang_c::ast::DeclaratorKind::Identifier;
+        use lang_c::visit::visit_init_declarator;
         if init_declarator.initializer.is_none() {
             match &init_declarator.declarator.node.kind.node {
                 Identifier(node) => {
-                    println!("{}", node.node.name.clone());
                     self.uninit.push(node.node.name.clone());
                 }
                 _ => {}
             }
         }
+        visit_init_declarator(self, init_declarator, span);
+    }
+
+    fn visit_binary_operator_expression(
+        &mut self,
+        binary_operator_expression: &'ast BinaryOperatorExpression,
+        span: &'ast Span
+    ) {
+        use lang_c::visit::visit_binary_operator_expression;
+        use lang_c::ast::Expression::Identifier;
+        match &binary_operator_expression.lhs.node {
+            Identifier(identifier) => {
+                self.uninit.retain(|n| n != &identifier.node.name);
+            }
+            _ => {}
+        }
+        visit_binary_operator_expression(self, binary_operator_expression, span);
+    }
+
+    fn visit_expression(
+        &mut self,
+        expression: &'ast lang_c::ast::Expression,
+        span: &'ast Span
+    ) {
+        use lang_c::ast::Expression::Identifier;
+        use lang_c::visit::visit_expression;
+        match &expression {
+            Identifier(identifier) => {
+                if self.uninit.contains(&identifier.node.name) {
+                    println!("{:?} Use before initialization: {}", &span, &identifier.node.name);
+                }
+            }
+            _ => {
+            }
+        }
+        visit_expression(self, expression, span);
     }
 }
