0a8f61decaded6776c9c0016b25a928fafa18a5c
Sam M W <you@example.com>
2022-11-16 22:07:28 +0000

    can now specify sourcefiles using command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use lang_c::driver::{parse, Config};
 
 mod unsafe_fn;
@@ -7,22 +9,25 @@ use crate::init::*;
 use lang_c::visit::Visit;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let config = Config::default();
-    let past = parse(&config, "c/unsafe_functions.c");
-    if let Ok(ref ast) = past {
-        let mut unsafe_fn: UnsafeFnCall = Default::default();
-        unsafe_fn.visit_translation_unit(&ast.unit);
-        for i in unsafe_fn.problems {
-            println!("{:?}: {}", i.0, i.1);
-        }
+    for file in args {
+        let past = parse(&config, file);
+        if let Ok(ref ast) = past {
+            let mut unsafe_fn: UnsafeFnCall = Default::default();
+            unsafe_fn.visit_translation_unit(&ast.unit);
+            for i in unsafe_fn.problems {
+                println!("{:?}: {}", i.0, i.1);
+            }
 
-        let mut init: VarInitCheck = Default::default();
-        init.visit_translation_unit(&ast.unit);
-        for i in init.problems {
-            println!("{:?}: {}", i.0, i.1);
+            let mut init: VarInitCheck = Default::default();
+            init.visit_translation_unit(&ast.unit);
+            for i in init.problems {
+                println!("{:?}: {}", i.0, i.1);
+            }
         }
-    }
-    if let Err(err) = past {
-        println!("{:?}", err);
+        if let Err(err) = past {
+            println!("{:?}", err);
+        }
     }
 }
