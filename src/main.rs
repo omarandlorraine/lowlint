965b43e9011076a069197137d03f1e3a014b5d4b
Sam M W <smw@alertergroup.co.uk>
2022-11-17 13:46:54 +0000

    don't try to open preprocessor options as files

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,31 @@ use crate::init::*;
 use lang_c::visit::Visit;
 
 fn main() {
+
+    fn arg4pp(arg: &String) -> bool {
+        if &arg[..2] == "-D" {
+            return true;
+        }
+        if &arg[..2] == "-I" {
+            return true;
+        }
+        false
+    }
+
     let args: Vec<String> = env::args().collect();
+    let mut files: Vec<String> = vec![];
 
     let mut config = Config::default();
 
     for arg in &args {
-        if &arg[..2] == "-D" {
+        if arg4pp(&arg) {
             config.cpp_options.push(arg.clone());
-        }
-        if &arg[..2] == "-I" {
-            config.cpp_options.push(arg.clone());
+        } else {
+            files.push(arg.clone());
         }
     }
     
-    for file in args {
+    for file in files {
         let past = parse(&config, file);
         if let Ok(ref ast) = past {
             let mut unsafe_fn: UnsafeFnCall = Default::default();
